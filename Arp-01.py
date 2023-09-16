40766cd1ef8b7ae1b9df95cc8a62e1e1d22ccf0f
verezeeee <theusma58@gmail.com>
2023-09-16 18:16:10 -0300

    first commit

---
+++ Arp-01.py
@@ -0,0 +1,17 @@
+# Exemplo 1: Resposta correta
+pergunta1 = "Qual é o resultado de 5 + 3?"
+resposta1 = input(pergunta1)
+
+if resposta1 == "8":
+    print("Parabéns, você acertou!")
+else:
+    print("Ops, parece que sua resposta está incorreta. Aqui está uma dica: A soma de 5 e 3 é igual a 8.")
+
+# Exemplo 2: Resposta incorreta
+pergunta2 = "Qual é a raiz quadrada de 16?"
+resposta2 = input(pergunta2)
+
+if resposta2 == "4":
+    print("Parabéns, você acertou!")
+else:
+    print("Ops, parece que sua resposta está incorreta. Aqui está uma dica: A raiz quadrada de 16 é igual a 4.")
